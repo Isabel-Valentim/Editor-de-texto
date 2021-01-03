0a346279f7ff1a97c0dc09df13bcfc0560e3818f
Isabel-Valentim <76108746+Isabel-Valentim@users.noreply.github.com>
2021-01-03 15:17:57 -0300

    functions with mistake

---
+++ scripteditortexto.js
@@ -0,0 +1,17 @@
+var texto = document.getElementById('texto')
+var res = document.getElementById('resultado')
+function título(){
+
+}
+function maiúscula(){
+res= texto.toUpperCase()
+}
+function minúscula(){
+
+}
+function negrito(){
+
+}
+function itálico(){
+
+}
